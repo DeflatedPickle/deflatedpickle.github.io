700766da6743e72ba3d4688411ed7e8cb7f66550
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-03 20:57:24 +0000

    Added some if statements for values

--- js/index.js
+++ js/index.js
@@ -2,5 +2,13 @@ document.addEventListener("keydown", function(event) {
     event = event || window.Event;
     if (event.keyCode == 13) {
         value = document.getElementById("insert").value
+        
+        if (value == 1) {
+            // About
+        } else if (value == 2) {
+            // Projects
+        } else if (value == 3) {
+            // Exit
+        }
     }
 })
