a1d9b3c8e3aa15d8a34339fca6189a42b90fb5e8
Dibbo <DeflatedPickle@gmail.com>
2017-12-04 09:34:32 +0000

    Updated to follow ESLint

--- js/index.js
+++ js/index.js
@@ -1,18 +1,43 @@
-document.addEventListener("keydown", function(event) {
+function insertParagraph(text) {
+    "use strict";
+    var element = document.createElement("p");
+    element.innerHTML = text;
+    document.getElementById("new").appendChild(element);
+}
+
+var list_count = 0;
+
+function insertList(list) {
+    "use strict";
+    var list_element = document.createElement("ul");
+    list_element.id = "list" + list_count;
+    document.getElementById("new").appendChild(list_element);
+    
+    list.forEach(function (item, index, array) {
+        var list_item = document.createElement("li");
+        list_item.innerHTML = "[" + (index + 1) + "] " + item;
+        document.getElementById(list_element.id).appendChild(list_item);
+    });
+    
+    list_count += 1;
+}
+
+document.addEventListener("keydown", function (event) {
+    "use strict";
     event = event || window.Event;
-    if (event.keyCode == 13) {
-        var value = document.getElementById("insert").value;
+    if (event.keyCode === 13) {
+        var value = parseInt(document.getElementById("insert").value);
         
         insertParagraph(value);
         
-        if (value == 1) {
+        if (value === 1) {
             // About
             insertParagraph("Retrieving about.");
-        } else if (value == 2) {
+        } else if (value === 2) {
             // Projects
             insertParagraph("Retrieving projects.");
             insertList(["pkinter", "quill"]);
-        } else if (value == 3) {
+        } else if (value === 3) {
             // Exit
             insertParagraph("Exiting the website.");
             window.close();
@@ -22,26 +47,4 @@ document.addEventListener("keydown", function(event) {
         
         document.getElementById("insert").value = "";
     }
-})
-
-function insertParagraph(text) {
-    var element = document.createElement("p");
-    element.innerHTML = text;
-    document.getElementById("new").appendChild(element);
-}
-
-var list_count = 0
-
-function insertList(list) {
-    var list_element = document.createElement("ul");
-    list_element.id = "list" + list_count;
-    document.getElementById("new").appendChild(list_element);
-    
-    list.forEach(function(item, index, array) {
-        var list_item = document.createElement("li");
-        list_item.innerHTML = "[" + (index + 1) + "] " + item;
-        document.getElementById(list_element.id).appendChild(list_item);
-    });
-    
-    list_count += 1;
-}
+});
